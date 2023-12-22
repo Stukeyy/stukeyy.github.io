805ae0ca0cc0e8be810180ee9f3e6a0068078d84
Stephen Ross <stephenr.ross@yahoo.com>
2023-12-22 11:20:49 +0000

    committee page and list added with a small easter egg for izzy. All content provided by Jane has now been incorporated and site at good foundation for next year. Merge to main, deploy to pages, notify Jane and then incorporate feedback

--- script.js
+++ script.js
@@ -1,47 +1,60 @@
-document.addEventListener('DOMContentLoaded',function(event){
-    // array with texts to type in typewriter
-    var dataText = [ "UKCI 2024", "23rd UK Workshop on Computational Intelligence", "Belfast, Northern Ireland, 4th - 6th September 2024", "Registration Opens 20th May 2024"];
-    
-    // type one text in the typwriter
-    // keeps calling itself until the text is finished
-    function typeWriter(text, i, fnCallback) {
-      // check if text isn't finished yet
-      if (i < (text.length)) {
-        // add next character to h1
-       document.getElementById("ukciannouncement").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
-  
-        // wait for a while and call this function again for next character
-        setTimeout(function() {
-          typeWriter(text, i + 1, fnCallback)
-        }, 100);
-      }
-      // text finished, call callback if there is a callback function
-      else if (typeof fnCallback == 'function') {
-        // call callback after timeout
-        setTimeout(fnCallback, 700);
-      }
-    }
-    // start a typewriter animation for a text in the dataText array
-     function StartTextAnimation(i) {
-       if (typeof dataText[i] == 'undefined'){
-          setTimeout(function() {
-            StartTextAnimation(0);
-          }, 5000);
-       }
-       // check if dataText[i] exists
-      if (i < dataText[i].length) {
-        // text exists! start typewriter animation
-       typeWriter(dataText[i], 0, function(){
-         // after callback (and whole text has been animated), start next text
-         StartTextAnimation(i + 1);
-       });
-      }
-    }
-    // start the text animation
-    StartTextAnimation(0);
-  });
+document.addEventListener('DOMContentLoaded', function (event) {
+  // array with texts to type in typewriter
+  var dataText = ["UKCI 2024", "23rd UK Workshop on Computational Intelligence", "Belfast, Northern Ireland, 4th - 6th September 2024", "Registration Opens 20th May 2024"];
 
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // check if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+      document.getElementById("ukciannouncement").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
 
-  // $( "#test" ).on( "click", function( event ) {
-  //   alert("HELLO");
-  // });
+      // wait for a while and call this function again for next character
+      setTimeout(function () {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+  function StartTextAnimation(i) {
+    if (typeof dataText[i] == 'undefined') {
+      setTimeout(function () {
+        StartTextAnimation(0);
+      }, 5000);
+    }
+    // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+      typeWriter(dataText[i], 0, function () {
+        // after callback (and whole text has been animated), start next text
+        StartTextAnimation(i + 1);
+      });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
+});
+
+const alertTrigger = document.getElementById('egg')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you triggered this alert message!', 'success')
+  })
+}
+
+$("#egg").click(function () {
+  function showTopAlert() {
+    var scrollTop = $(this).scrollTop()
+    $('#topAlertContainer').css('top', scrollTop + 10).fadeIn().delay(2000).fadeOut();
+  }
+
+  // Call function to show the alert
+  showTopAlert();
+});
+
